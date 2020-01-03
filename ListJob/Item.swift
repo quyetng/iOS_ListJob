00f2b368e11e8c76e2b97e851d5598a7885af85b
QuyetNguyen <quyetnguyentan@gmail.com>
2020-01-02 23:30:27 -0700

    Add category

---
+++ ListJob/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  ListJob
+//
+//  Created by QN on 1/2/20.
+//  Copyright © 2020 QN. All rights reserved.
+//
+
+import Foundation
---
+++ ListJob/Models/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  ListJob
+//
+//  Created by QN on 1/2/20.
+//  Copyright © 2020 QN. All rights reserved.
+//
+
+import Foundation
